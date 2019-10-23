a9877cc317713616ecc6df26d3637f927c539cda
Erialdo Meta <erialdo.meta@gmail.com>
2019-10-24 01:35:47 +0200

    wrapping third party services

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { EventService } from './shared/event.service';  
+import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service';  
+
 
 @Component({
     selector: 'events-list',
@@ -9,7 +11,7 @@ import { EventService } from './shared/event.service';
         <hr />
         <div class="row">
             <div *ngFor="let event of events" class="col-md-5">
-                <event-thumbnail [event]="event"></event-thumbnail>
+                <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
             </div>
         </div>
     </div>
@@ -19,10 +21,14 @@ import { EventService } from './shared/event.service';
 export class EventsListComponent implements OnInit{
   events: any[]
 
-  constructor(private eventService: EventService) {
+  constructor(private eventService: EventService, private toastr: ToastrService) {
   }
   
   ngOnInit()  {
     this.events = this.eventService.getEvents()
   }
+
+  handleThumbnailClick(eventName) {
+    this.toastr.success(eventName)
+  }  
 }
