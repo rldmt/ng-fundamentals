9ae145c203d423946c2853538477558eaa338a14
Erialdo Meta <erialdo.meta@gmail.com>
2019-10-25 01:33:41 +0200

    Accessing Route params

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { EventService } from "../shared/event.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     templateUrl: './event-details.component.html',
@@ -11,11 +12,11 @@ import { EventService } from "../shared/event.service";
 
 export class EventDetailsComponent  {
     event: any
-    constructor(private eventService: EventService) {
+    constructor(private eventService: EventService, private route: ActivatedRoute) {
 
     }
 
     ngOnInit() {
-        this.event = this.eventService.getEvent(1)
+        this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
     }
 }
