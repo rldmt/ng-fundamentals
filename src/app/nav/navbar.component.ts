01f33d2afda3a1165f8efc8c066a150dfbd517a6
Erialdo Meta <erialdo.meta@gmail.com>
2019-10-28 17:10:35 +0100

    Styling Active Links

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -3,9 +3,11 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'nav-bar',
     templateUrl:'./navbar.component.html',
-    styles: [` .nav.navbar-nav {font-size: 15px;}
-               #searchForm {margin-right: 100px}
-               @media (max-width:1200 px) {#searchForm {display: none;}}
+    styles: [`
+        .nav.navbar-nav {font-size: 15px;}
+        #searchForm {margin-right: 100px}
+        @media (max-width:1200 px) {#searchForm {display: none;}}
+        li > a.active { color: #F97924;  }
     `]
 })
 
