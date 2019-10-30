99d2feb51a097bc571e76a588147fb5b8fccdad2
rldmt <19992681+rldmt@users.noreply.github.com>
2019-10-30 14:06:39 +0100

    login form validation

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,12 +3,16 @@ import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styles: [`
+        em { float:right; color:#E05C65; padding-left:10px;  }
+    `]
 })
 
 export class LoginComponent {
     username
     password
+    mouseoverLogin
     constructor(private authService:AuthService, private router: Router)    {
 
     }
