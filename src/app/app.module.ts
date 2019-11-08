cee9ee0a185ac61f7a053c7860a670b3d994345a
rldmt <19992681+rldmt@users.noreply.github.com>
2019-11-08 11:28:36 +0100

    create event form with reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,10 +19,13 @@ import { ToastrService } from './common/toastr.service';
 import { appRoutes } from './routes';
 import { Error404Component } from './events/errors/404.component';
 import { AuthService } from './user/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   declarations: [
