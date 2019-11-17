b1130bd6994b3b424d72337bf9f92d9c472456a0
Erialdo Meta <erialdo.meta@gmail.com>
2019-11-17 18:24:33 +0100

    collapsible well

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { appRoutes } from './routes';
 import { Error404Component } from './events/errors/404.component';
 import { AuthService } from './user/auth.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CollapsibleWell } from './common/collapsible-well.component';
 
 @NgModule({
   imports: [
@@ -39,7 +40,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CreateEventComponent,
     CreateSessionComponent,
     Error404Component,
-    SessionListComponent
+    SessionListComponent,
+    CollapsibleWell
   ],
   providers: [
     EventService, 
