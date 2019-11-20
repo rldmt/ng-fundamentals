551f5417d16f9f9a03183e487bad4f5d65feab01
Erialdo Meta <erialdo.meta@gmail.com>
2019-11-20 21:13:49 +0100

    filtering data

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 import { ISession } from '../shared/index';
 
 @Component({
@@ -6,6 +6,22 @@ import { ISession } from '../shared/index';
     templateUrl: './session-list.component.html'
 })
 
-export class SessionListComponent {
-    @Input() sessions:ISession[]
+export class SessionListComponent implements OnChanges {
+    @Input() sessions: ISession[];
+    @Input() filterBy: string;
+    visibleSessions: ISession[] = [];
+
+    ngOnChanges() {
+        if (this.sessions) {
+            this.filterSessions(this.filterBy);
+        }
+    }
+
+    filterSessions(filterValue) {
+        if (filterValue === 'all') {
+            this.visibleSessions = this.sessions.slice(0);
+        } else {
+            this.visibleSessions = this.sessions.filter(session => { return session.level.toLocaleLowerCase() === filterValue; })
+        }
+    }
 }
