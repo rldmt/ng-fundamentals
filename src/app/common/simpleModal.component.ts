4ed9bd502e156b9c2aceabcedcd3940112f69ca4
Erialdo Meta <erialdo.meta@gmail.com>
2019-12-07 16:18:26 +0100

    close on body false setting

--- src/app/common/simpleModal.component.ts
+++ src/app/common/simpleModal.component.ts
@@ -25,6 +25,7 @@ import { JQ_TOKEN } from './jQuery.service';
 export class SimpleModalComponent {
     @Input() title: string;
     @Input() elementId: string;
+    @Input() closeOnBodyClick: string;
     @ViewChild('modalcontainer') containerEl: ElementRef; 
 
     constructor(@Inject(JQ_TOKEN) private $: any) {
@@ -32,6 +33,8 @@ export class SimpleModalComponent {
     }
 
     closeModal() {
-        this.$(this.containerEl.nativeElement).modal('hide');
+        if(this.closeOnBodyClick.toLocaleLowerCase() === 'true') {
+            this.$(this.containerEl.nativeElement).modal('hide');
+        }
     }
 }
