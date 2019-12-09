1dac20c7f0614415b98a0efc1085bb9eac035e6e
Erialdo Meta <erialdo.meta@gmail.com>
2019-12-09 23:46:32 +0100

    creating server and connecting httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from "@angular/common/http";
 
 import {
   EventsListComponent,
@@ -33,7 +34,8 @@ let jQuery = window['$'];
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   declarations: [
     EventsAppComponent,
