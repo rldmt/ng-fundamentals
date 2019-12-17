c511a6f1861778aa9d5621c1288258d7a438e871
Erialdo Meta <erialdo.meta@gmail.com>
2019-12-17 02:22:37 +0100

    Testing components with shallow integrated tests

--- src/app/events/event-details/session-list.component.integrated.spec.ts
+++ src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import { DebugElement } from "@angular/core";
+import { DebugElement, NO_ERRORS_SCHEMA } from "@angular/core";
 import { SessionListComponent } from './session-list.component';
 import { AuthService } from '../../user/auth.service';
 import { VoterService } from './voter.service';
@@ -7,7 +7,7 @@ import { ISession } from "../shared/event.model";
 import { By } from "@angular/platform-browser";
 import { UpvoteComponent } from './upvote.component';
 import { DurationPipe } from '../shared';
-import { CollapsibleWell } from 'src/app/common';
+import { CollapsibleWell } from '../../common/collapsible-well.component';
 
 describe('SessionListComponent', () => {
     let fixture: ComponentFixture<SessionListComponent>,
@@ -28,15 +28,17 @@ describe('SessionListComponent', () => {
             imports: [],
             declarations: [
                 SessionListComponent,
-                UpvoteComponent,
+                //UpvoteComponent,
                 DurationPipe,
-                CollapsibleWell
+                //CollapsibleWellComponent
             ],
             providers: [
                 { provide: AuthService, useValue: mockAuthService },
                 { provide: VoterService, useValue: mockVoterService }
             ],
-            schemas: []
+            schemas: [
+                NO_ERRORS_SCHEMA
+            ]
         })
     }))
 
